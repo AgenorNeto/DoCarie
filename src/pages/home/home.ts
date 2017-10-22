3c6bfcf38597439cad9b7df619a297d9694f47d2
Victor Almeida Schinaider <victorschinaider@outlook.com>
2017-10-22 07:16:21 -0300

    Notificação teste

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { UserInfoPage } from '../user-info/user-info';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 @Component({
   selector: 'page-home',
@@ -8,10 +9,24 @@ import { UserInfoPage } from '../user-info/user-info';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {  }
+  constructor(
+    public navCtrl: NavController,
+    private localNotifications: LocalNotifications
+  ) {  }
 
   openQuestions() {
     this.navCtrl.push(UserInfoPage);
+  }
+
+  ionViewDidLoad() {
+    this.localNotifications.schedule({
+      id: 1,
+      title: 'Notification',
+      text: 'Single ILocalNotification',
+      sound: true/*isAndroid*/? 'file://sound.mp3': 'file://beep.caf',
+      data: { secret: 'thejey' }
+    });
+    console.log('Go notification!');
   }
 
 }
